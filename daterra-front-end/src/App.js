27ebcd18ea07c789c2b87c162f54d5584fb04d5d
Felipe Agustini <felipeboccardi@hotmail.com>
2022-04-04 21:15:23 -0300

    Iniciando projeto em React

---
+++ daterra-front-end/src/App.js
@@ -0,0 +1,15 @@
+import React, {useEffect, useState} from "react";
+import Login from "./cointainers/Login/Login";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App-background">
+      <header className="App-header">
+        <Login></Login>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ daterra-front-end/src/cointainers/Login/Login.js
@@ -0,0 +1,9 @@
+import React, {useEffect, useState} from "react";
+import './Login.css';
+
+function Login() {
+    return (
+        <div>Login</div>
+      );
+  }
+export default Login;
