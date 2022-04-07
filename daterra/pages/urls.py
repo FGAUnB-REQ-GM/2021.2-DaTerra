5bb21a42cfee687bcd734db41f817fec97781ae3
José Guilherme <joseguilhermefmoura@gmail.com>
2022-04-07 11:52:12 -0300

    :arrow_up: Preparando para deploy

--- daterra/pages/urls.py
+++ daterra/pages/urls.py
@@ -1,7 +1,8 @@
 from ast import Index
-from .views import index
+from .views import *
 from django.urls import path
 
 urlpatterns = [
     path('', index, name='index'),
+    path('user_choice', user_choice, name='user_choice'),
 ]
--- daterra/pages/views.py
+++ daterra/pages/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'pages/home.html')
+
+def user_choice(request):
+    return render(request, 'pages/user_choice.html')
