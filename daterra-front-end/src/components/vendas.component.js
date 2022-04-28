c92d03073b885ee9d1fe2b9cf07ccff54923ff1e
Felipe Agustini <felipeboccardi@hotmail.com>
2022-04-27 22:39:51 -0300

    Adicionando modal de feedback

--- daterra-front-end/src/components/vendas.component.js
+++ daterra-front-end/src/components/vendas.component.js
@@ -2,8 +2,12 @@ import React, { Component } from "react";
 import Navbar from "./navbarclient.component";
 import Food from "./../assets/images/food.jpg"
 import { BsFillStarFill } from "react-icons/bs";
+import SearchBar from "./pesquisa.component";
+
+
 
 const array = [{
+    id: 1,
     product_name: "Lorem Ipsum",
     tot_comprado: "150kg",
     num_contat: "9123910293",
@@ -11,7 +15,7 @@ const array = [{
     image: Food,
     score: null,
 },
-{
+{id: 2,
     product_name: "Lorem Ipsum",
     tot_comprado: "150kg",
     num_contat: "9123910293",
@@ -19,7 +23,7 @@ const array = [{
     image: Food,
     score: 3,
 },
-{
+{id: 3,
     product_name: "Lorem Ipsum",
     tot_comprado: "150kg",
     num_contat: "9123910293",
@@ -28,7 +32,7 @@ const array = [{
     score: 1,
 }
 ]
-function scoreStars(score){
+function scoreStars(score,id){
     if(score === 5){
         return(
             <div class="col-lg-2">
@@ -122,13 +126,16 @@ function scoreStars(score){
     }else{
         return(
         <div class="col-lg-2">
-                <a class="a-link no-sub ps-5"href="">
+
+                <a class="a-link no-sub ps-5" type="button" data-bs-toggle="modal" data-bs-target={`#modal`+id} >
                     finalizar pedido
                 </a>
+                
         </div>
         )
     }
 }
+
 const itens = array.map((element) =>
     <div>
         <div class="farm-product col-lg-12"> 
@@ -143,23 +150,42 @@ const itens = array.map((element) =>
                 Cliente: {element.num_contat} <br/>
                 Valor total: {element.total_price}
             </div>
-            {scoreStars(element.score)}
+            {scoreStars(element.score, element.id)}
         </div>
         <div class="vl-horizontal col-lg-8">
+        </div>
+
+        <div class="modal fade" id={`modal`+element.id} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Finalizar Pedido</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <input type="number" min="0" max="5" placeholder="0"></input>
+            </div>
+            <div class="modal-footer text-center">
+                <button type="button" class=" btn btn-primary" data-bs-dismiss="modal">Finalizar</button>
+            </div>
+            </div>
+        </div>
         </div>
 </div>
 );
 
 export default class SignUp extends Component {
-    
+   
     render() {
         return (
             <div >
                 <Navbar/>
+
                 <div className="white-box col-lg-10" > 
-                    <div class="vl-horizontal col-lg-8"></div>
-                    {itens}
-                </div>
+                    <SearchBar/>
+                        <div class="vl-horizontal col-lg-8"></div>
+                        {itens}
+                    </div>
             </div>
         );
     }
