938376c8c2bc06a547bcceb4f1b4cf1ebb000d25
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-04-27 21:41:01 -0300

    :arrow_up: Atualizacao nas paginas

--- daterra/pages/urls.py
+++ daterra/pages/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', index, name='index'),
+    path('cliente/home', cliente_home, name="cliente_home"),
     path('user_choice', user_choice, name='user_choice'),
 ]
--- daterra/pages/views.py
+++ daterra/pages/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-    return render(request, 'pages/home.html')
+    if request.user.is_authenticated:
+        return render(request, 'pages/cliente/home.html')
+    else:
+        return render(request, 'registration/login.html')
 
 def user_choice(request):
     return render(request, 'pages/user_choice.html')
+
+def cliente_home(request):
+    return render(request, 'pages/cliente/home.html')
