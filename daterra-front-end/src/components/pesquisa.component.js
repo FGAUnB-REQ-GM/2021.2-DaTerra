10a8d93a2101c19bd74928832090196e636d379d
Felipe Agustini <felipeboccardi@hotmail.com>
2022-04-27 21:23:50 -0300

    Adiconando estrelinhas nas vendas

---
+++ daterra-front-end/src/components/pesquisa.component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const searchbar = () => {
+    return(
+      
+        <div>
+            <input class="search-bar"  type="text" placeholder="Pesquisar...">
+            </input>
+        </div>
+    );
+}
+export default (searchbar);
--- daterra-front-end/src/components/vendas.component.js
+++ daterra-front-end/src/components/vendas.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "./navbarclient.component";
-import Add from "./Buttons/addbutton.component";
 import Food from "./../assets/images/food.jpg"
+import { BsFillStarFill } from "react-icons/bs";
 
 const array = [{
     product_name: "Lorem Ipsum",
@@ -9,6 +9,7 @@ const array = [{
     num_contat: "9123910293",
     total_price: "23,98",
     image: Food,
+    score: null,
 },
 {
     product_name: "Lorem Ipsum",
@@ -16,6 +17,7 @@ const array = [{
     num_contat: "9123910293",
     total_price: "23,98",
     image: Food,
+    score: 3,
 },
 {
     product_name: "Lorem Ipsum",
@@ -23,53 +25,117 @@ const array = [{
     num_contat: "9123910293",
     total_price: "23,98",
     image: Food,
-},
-{
-    product_name: "Lorem Ipsum",
-    tot_comprado: "150kg",
-    num_contat: "9123910293",
-    total_price: "23,98",
-    image: '',
-},{
-    product_name: "Lorem Ipsum",
-    tot_comprado: "150kg",
-    num_contat: "9123910293",
-    total_price: "23,98",
-    image: '',
-},{
-    product_name: "Lorem Ipsum",
-    tot_comprado: "150kg",
-    num_contat: "9123910293",
-    total_price: "23,98",
-    image: '',
-},{
-    product_name: "Lorem Ipsum",
-    tot_comprado: "150kg",
-    num_contat: "9123910293",
-    total_price: "23,98",
-    image: '',
-},{
-    product_name: "Lorem Ipsum",
-    tot_comprado: "150kg",
-    num_contat: "9123910293",
-    total_price: "23,98",
-    image: '',
-},{
-    product_name: "Lorem Ipsum",
-    tot_comprado: "150kg",
-    num_contat: "9123910293",
-    total_price: "23,98",
-    image: '',
+    score: 1,
 }
 ]
-
+function scoreStars(score){
+    if(score === 5){
+        return(
+            <div class="col-lg-2">
+                <div>
+                <i class="bi bi-star-fill star-size checked"> <BsFillStarFill/></i>
+                <i class="bi bi-star-fill star-size checked"> <BsFillStarFill/></i>
+                <i class="bi bi-star-fill star-size checked"> <BsFillStarFill/></i>
+                <i class="bi bi-star-fill star-size checked"> <BsFillStarFill/></i>
+                <i class="bi bi-star-fill star-size checked"> <BsFillStarFill/></i>
+                </div>
+                <span class="ps-5">
+                    pedido finalizado
+                </span>
+            </div>
+        )
+    }else if(score === 4){
+        return(
+            <div class="col-lg-2">
+                <div>
+                <i class="bi bi-star-fill star-size checked"> <BsFillStarFill/></i>
+                <i class="bi bi-star-fill star-size checked"> <BsFillStarFill/></i>
+                <i class="bi bi-star-fill star-size checked" > <BsFillStarFill/></i>
+                <i class="bi bi-star-fill star-size checked"> <BsFillStarFill/></i>
+                <i class="bi bi-star-fill star-size"> <BsFillStarFill/></i>
+                </div>
+                <span class="ps-5">
+                    pedido finalizado
+                </span>
+            </div>
+        )
+    }else if(score === 3){
+        return(
+            <div class="col-lg-2">
+                <div>
+                <i class="bi bi-star-fill star-size checked"> <BsFillStarFill/></i>
+                <i class="bi bi-star-fill star-size checked"> <BsFillStarFill/></i>
+                <i class="bi bi-star-fill star-size checked" > <BsFillStarFill/></i>
+                <i class="bi bi-star-fill star-size"> <BsFillStarFill/></i>
+                <i class="bi bi-star-fill star-size"> <BsFillStarFill/></i>
+                </div>
+                <span class="ps-5">
+                    pedido finalizado
+                </span>
+            </div>
+        )
+    }else if(score === 2){
+        return(
+            <div class="col-lg-2">
+                <div>
+                <i class="bi bi-star-fill star-size checked"> <BsFillStarFill/></i>
+                <i class="bi bi-star-fill star-size checked"> <BsFillStarFill/></i>
+                <i class="bi bi-star-fill star-size" > <BsFillStarFill/></i>
+                <i class="bi bi-star-fill star-size"> <BsFillStarFill/></i>
+                <i class="bi bi-star-fill star-size"> <BsFillStarFill/></i>
+                </div>
+                <span class="ps-5">
+                    pedido finalizado
+                </span>
+            </div>
+        )
+    }else if(score === 1){
+        return(
+            <div class="col-lg-2">
+                <div>
+                <i class="bi bi-star-fill star-size checked"> <BsFillStarFill/></i>
+                <i class="bi bi-star-fill star-size"> <BsFillStarFill/></i>
+                <i class="bi bi-star-fill star-size" > <BsFillStarFill/></i>
+                <i class="bi bi-star-fill star-size"> <BsFillStarFill/></i>
+                <i class="bi bi-star-fill star-size"> <BsFillStarFill/></i>
+                </div>
+                <span class="ps-5">
+                    pedido finalizado
+                </span>
+            </div>
+        )
+    }else if(score === 0){
+        return(
+            <div class="col-lg-2">
+                <div>
+                    <i class="bi bi-star-fill star-size"> <BsFillStarFill/></i>
+                    <i class="bi bi-star-fill star-size"> <BsFillStarFill/></i>
+                    <i class="bi bi-star-fill star-size"> <BsFillStarFill/></i>
+                    <i class="bi bi-star-fill star-size"> <BsFillStarFill/></i>
+                    <i class="bi bi-star-fill star-size"> <BsFillStarFill/></i>
+                </div>
+                <span class="ps-5">
+                    pedido finalizado
+                </span>
+            </div>
+        )
+    }else{
+        return(
+        <div class="col-lg-2">
+                <a class="a-link no-sub ps-5"href="">
+                    finalizar pedido
+                </a>
+        </div>
+        )
+    }
+}
 const itens = array.map((element) =>
     <div>
         <div class="farm-product col-lg-12"> 
             <div class="col-lg-4" >
                 <img className="img-element" src={element.image}/>
             </div>
-            <div class="line col-lg-3">
+            <div class="line col-lg-2">
                 {element.product_name}<br/>
                 Quantidade comprada: {element.tot_comprado}<br/>
             </div>
@@ -77,14 +143,10 @@ const itens = array.map((element) =>
                 Cliente: {element.num_contat} <br/>
                 Valor total: {element.total_price}
             </div>
-            <div class="col-lg-2">
-                <a class="a-link no-sub "href="">
-                    finalizar pedido
-                </a>
+            {scoreStars(element.score)}
         </div>
-    </div>
-
-<div class="vl-horizontal col-lg-8"></div>
+        <div class="vl-horizontal col-lg-8">
+        </div>
 </div>
 );
 
